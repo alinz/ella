470caa367fd6b9d919982919354d9c471c47d023
Ali Najafizadeh <a.najafizadeh@gmail.com>
2023-02-07 02:06:47 -0500

    Add one more test to constant

--- schema/parser/constant_test.go
+++ schema/parser/constant_test.go
@@ -7,6 +7,10 @@ import (
 func TestConstantParser(t *testing.T) {
 	runTests(t, TestCases{
 		{
+			Input:  `rpc = 1.0.0-b01`,
+			Output: `rpc = 1.0.0-b01`,
+		},
+		{
 			Input: `a = 1 b= 2`,
 			Output: `
 a = 1
