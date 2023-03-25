3d148f20d472fb30889b3506d06c9413f8d96954
Ali Najafizadeh <a.najafizadeh@gmail.com>
2023-03-25 18:28:10 -0400

    Add some enhancedment to the restclient transformer

--- transform/restclient/constant.go
+++ transform/restclient/constant.go
@@ -1,4 +1,4 @@
-package http
+package restclient
 
 import (
 	"ella.to/schema/ast"
@@ -9,14 +9,27 @@ func Constants(program *ast.Program) transform.Func {
 	return func(out transform.Writer) error {
 		constants := ast.GetSlice[*ast.Constant](program)
 
+		isHostnameDefined := false
+		isPortDefined := false
+
 		for _, constant := range constants {
 			switch constant.Name.Name {
 			case "http_hostname":
 				out.Str("@hostname = %s", constant.Value.(*ast.ValueString).Content).Lines(1)
+				isHostnameDefined = true
 			case "http_port":
 				out.Str("@port = %s", constant.Value.(*ast.ValueString).Content).Lines(1)
+				isPortDefined = true
 			}
 		}
+
+		if !isHostnameDefined {
+			out.Str("@hostname = ").Lines(1)
+		}
+		if !isPortDefined {
+			out.Str("@port = ").Lines(1)
+		}
+
 		out.Str("@host = {{hostname}}:{{port}}").Lines(1)
 		out.Str("@authorization = ")
 
