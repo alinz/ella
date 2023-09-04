68e16e937d07a58153cae20bed486ad4f83c4592
Ali Najafizadeh <a.najafizadeh@gmail.com>
2023-09-04 13:20:11 -0400

    Add e2e for fileupload

---
+++ e2e/upload/upload.go
@@ -0,0 +1,32 @@
+package upload
+
+import (
+	"context"
+	"errors"
+	"io"
+)
+
+type HttpStorageServiceImpl struct {
+}
+
+var _ HttpStorageService = (*HttpStorageServiceImpl)(nil)
+
+func (s *HttpStorageServiceImpl) UploadFiles(ctx context.Context, files func() (string, io.Reader, error), id string) (results []File, err error) {
+	results = make([]File, 0)
+
+	for {
+		filename, content, err := files()
+		if errors.Is(err, io.EOF) {
+			return results, nil
+		} else if err != nil {
+			return nil, err
+		}
+
+		size, _ := io.Copy(io.Discard, content)
+
+		results = append(results, File{
+			Name: filename,
+			Size: size,
+		})
+	}
+}
---
+++ e2e/upload/upload_test.go
@@ -0,0 +1,59 @@
+package upload
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpload(t *testing.T) {
+	server := httptest.NewServer(
+		CreateStorageServiceServer(&HttpStorageServiceImpl{}),
+	)
+
+	host := server.URL
+	httpClient := &http.Client{}
+
+	client := CreateHttpStorageServiceClient(host, httpClient)
+
+	files := make(chan *struct {
+		FileName string
+		Content  io.Reader
+	}, 2)
+	for i := 0; i < 2; i++ {
+		files <- &struct {
+			FileName string
+			Content  io.Reader
+		}{
+			FileName: fmt.Sprintf("test%d.txt", i),
+			Content:  strings.NewReader("Hello World"),
+		}
+	}
+	close(files)
+
+	results, err := client.UploadFiles(context.Background(), func() (string, io.Reader, error) {
+		file, ok := <-files
+		if !ok {
+			return "", nil, io.EOF
+		}
+
+		return file.FileName, file.Content, nil
+	}, "test")
+	assert.NoError(t, err)
+	assert.Equal(t, []File{
+		{
+			Name: "test0.txt",
+			Size: 11,
+		},
+		{
+			Name: "test1.txt",
+			Size: 11,
+		},
+	}, results)
+}
