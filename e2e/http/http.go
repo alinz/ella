e1a7b9546ff6330e6bfc3ec814bc58cd4d270d05
Ali Najafizadeh <a.najafizadeh@gmail.com>
2024-04-28 10:02:27 -0400

    Update e2e test

--- e2e/http/http.go
+++ e2e/http/http.go
@@ -2,7 +2,6 @@ package http
 
 import (
 	"context"
-	"net/http"
 )
 
 type HttpPeopleServiceImpl struct {
@@ -12,7 +11,7 @@ var _ HttpPeopleService = (*HttpPeopleServiceImpl)(nil)
 
 func (s *HttpPeopleServiceImpl) GetRandom(ctx context.Context, age int8) (person *Person, err error) {
 	if age < 0 {
-		return nil, Errorf(http.StatusBadRequest, nil, "age must be greater than 0")
+		return nil, ErrAgen
 	}
 
 	return &Person{
